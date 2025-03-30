9bef0a650544cff2e0b4913b00fd390dc7b6effa
boommerspdn <131756488+boommerspdn@users.noreply.github.com>
2025-03-30 08:18:00 +0700

    change prismadb

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,11 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = global as unknown as { prisma?: PrismaClient };
 
-const prismadb = globalThis.prisma ?? new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
+const prismadb = globalForPrisma.prisma ?? new PrismaClient();
 
 export default prismadb;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prismadb;
